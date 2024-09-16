4a2489c0303d5cc0a39fa14c1ea9b89db0086986
Sudoix <sudoix.com@gmail.com>
2024-09-16 14:18:16 +0200

    modify setup.sh

---
+++ setup.sh
@@ -0,0 +1,78 @@
+#! /usr/bin/env bash
+
+ENV=`dirname "$(readlink -f "$BASH_SOURCE")"`
+LN="ln -fs"
+
+# Install some packages 
+sudo apt install -y python3-pip python3-venv curl git screen bmon \
+  python3-full xclip ansible zsh-autosuggestions zsh-syntax-highlighting \
+  gnupg software-properties-common flameshot kazam vlc aptitude nload aria2 \
+  gcc make perl terminator jcal remina keepassxc p7zip-full rar unrar \
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Oh My Zsh Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install Vim plugins
+read -p "Do you want to install Vim plugins? [N/y] " 
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+  python3 -m venv ${HOME}/.vim/pyenv
+fi
+
+function linkfile {
+	if [ -f $2 ]; then
+		read -p "Do you want to overwrite sym-link: '$2'? [N/y] "
+		if [[ $REPLY =~ ^[Yy]$ ]]
+		then
+			echo "Overwriting: $2"
+		    # do dangerous stuff
+		else
+			return
+		fi
+	fi
+	$LN $1 $2
+}
+
+# zsh
+read -p "Do you want to update the .zshrc? [N/y] " 
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  linkfile "${ENV}/zshrc" "${HOME}/.zshrc"
+fi
+
+read -p "Do you want to update the .zsh_aliases? [N/y] " 
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  linkfile "${ENV}/zsh_aliases" "${HOME}/.zsh_aliases"
+fi
+
+# vim
+read -p "Do you want to create .vimrc? [N/y] "
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  linkfile "${ENV}/vimrc" "${HOME}/.vimrc"
+fi
+
+# default editor
+read -p "Do you want to set vim as the default editor? [N/y] "
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo 'export EDITOR=/usr/bin/vim' | sudo tee /etc/profile.d/editor.sh \
+  > /dev/null
+fi
+
+# ssh
+read -p "Do you want to update the SSH config? [N/y] "
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  mkdir -p "${HOME}/.ssh/sshcontrolmasters"
+  linkfile "${ENV}/sshconfig" "${HOME}/.ssh/config"
+fi
+
+# terminator
+read -p "Do you want to update the Terminator config and theme? [N/y] "
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  mkdir -p "${HOME}/.config/terminator"
+  linkfile "${ENV}/terminatorconfig" "${HOME}/.config/terminator/config"
+  linkfile "${ENV}/gtk.css" "${HOME}/.config/gtk-3.0/gtk.css"
+fi
