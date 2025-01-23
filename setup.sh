f66536d2189a770c930fc7acc7bb75d132b01133
Sudoix <sudoix.com@gmail.com>
2025-01-23 11:27:55 +0100

    add ipython3 package

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ sudo apt update && sudo apt install -y python3-pip python3-venv curl git tmux \
   python3-full xclip zsh-autosuggestions zsh-syntax-highlighting vim ncal tree\
   gnupg software-properties-common flameshot kazam vlc aptitude nload aria2 \
   gcc make perl terminator jcal remmina keepassxc p7zip-full rar unrar bmon \
-  gnome-tweaks gnome-shell-extension-manager
+  gnome-tweaks gnome-shell-extension-manager ipython3
 
 if [[ $? -ne 0 ]]; then
   echo "Some packages failed to install"
